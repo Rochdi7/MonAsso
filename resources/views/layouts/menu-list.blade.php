99de0fcbac0f11a69936bb47f855a21103dbc31d
Rochdi Karouali <rochdi.karouali1234@gmail.com>
2025-05-28 18:44:40 +0100

    add files of my proejct

---
+++ resources/views/layouts/menu-list.blade.php
@@ -0,0 +1,8 @@
+<li class="pc-item">
+  <a href="/index" class="pc-link">
+    <span class="pc-micon">
+      <i class="ph-duotone ph-gauge"></i>
+    </span>
+    <span class="pc-mtext" data-i18n="Dashboard">Dashboard</span>
+  </a>
+</li>
