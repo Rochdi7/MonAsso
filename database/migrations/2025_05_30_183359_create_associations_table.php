e449d9e91341839087b7f78d1a5255d6adb14838
Rochdi Karouali <rochdi.karouali1234@gmail.com>
2025-05-31 00:03:45 +0100

    add migrations for assosiations and members

---
+++ database/migrations/2025_05_30_183359_create_associations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('associations', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->string('address');
+            $table->string('email')->unique();
+            $table->text('logo')->nullable();
+            $table->text('announcement_status')->nullable();
+            $table->date('creation_date')->nullable();
+            $table->boolean('is_validated')->default(false);
+            $table->timestamp('validation_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('associations');
+    }
+};
---
+++ database/migrations/2025_05_30_183400_create_members_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->string('email')->nullable()->unique();
+            $table->text('password');
+            $table->string('phone');
+            $table->enum('role', ['admin', 'membre', 'super_admin']);
+            $table->text('availability')->nullable();
+            $table->text('skills')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->boolean('is_admin')->default(false);
+            $table->rememberToken();
+            $table->uuid('association_id');
+            $table->foreign('association_id')->references('id')->on('associations')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('members');
+    }
+};
