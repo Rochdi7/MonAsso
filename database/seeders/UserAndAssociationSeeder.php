691486ea5f7f444315362ed2ee1aa1b3b85bd220
Rochdi Karouali <rochdi.karouali1234@gmail.com>
2025-06-17 12:39:02 +0100

    updated user&permissions seeder

--- database/seeders/UserAndAssociationSeeder.php
+++ database/seeders/UserAndAssociationSeeder.php
@@ -12,7 +12,7 @@ class UserAndAssociationSeeder extends Seeder
     public function run(): void
     {
         // Create Associations
-        $asso1 = Association::create([
+        $assoSuper = Association::create([
             'name' => 'Asso Super',
             'email' => 'super@asso.com',
             'address' => '123 Rue Super',
@@ -20,48 +20,86 @@ class UserAndAssociationSeeder extends Seeder
             'creation_date' => now(),
         ]);
 
-        $asso2 = Association::create([
-            'name' => 'Asso Admin',
-            'email' => 'admin@asso.com',
-            'address' => '456 Rue Admin',
+        $assoAdmin1 = Association::create([
+            'name' => 'Asso Admin One',
+            'email' => 'admin1@asso.com',
+            'address' => '456 Rue Admin One',
             'announcement_status' => 'active',
             'creation_date' => now(),
         ]);
 
-        $asso3 = Association::create([
-            'name' => 'Asso Membre',
-            'email' => 'membre@asso.com',
-            'address' => '789 Rue Membre',
+        $assoAdmin2 = Association::create([
+            'name' => 'Asso Admin Two',
+            'email' => 'admin2@asso.com',
+            'address' => '789 Rue Admin Two',
             'announcement_status' => 'active',
             'creation_date' => now(),
         ]);
 
-        // Create Users and assign roles
+        // Create Super Admin (can access everything)
         $super = User::create([
             'name' => 'Super Admin',
             'email' => 'super@asso.com',
             'phone' => '0600000001',
             'password' => Hash::make('password'),
-            'association_id' => $asso1->id,
+            'association_id' => $assoSuper->id,
         ]);
         $super->assignRole('super_admin');
 
-        $admin = User::create([
-            'name' => 'Association Admin',
-            'email' => 'admin@asso.com',
+        // Create Admin 1
+        $admin1 = User::create([
+            'name' => 'Admin One',
+            'email' => 'admin1@asso.com',
             'phone' => '0600000002',
             'password' => Hash::make('password'),
-            'association_id' => $asso2->id,
+            'association_id' => $assoAdmin1->id,
         ]);
-        $admin->assignRole('admin');
+        $admin1->assignRole('admin');
 
-        $membre = User::create([
-            'name' => 'Regular Member',
-            'email' => 'membre@asso.com',
+        // Create Admin 2
+        $admin2 = User::create([
+            'name' => 'Admin Two',
+            'email' => 'admin2@asso.com',
             'phone' => '0600000003',
             'password' => Hash::make('password'),
-            'association_id' => $asso3->id,
+            'association_id' => $assoAdmin2->id,
         ]);
-        $membre->assignRole('membre');
+        $admin2->assignRole('admin');
+
+        // Create Membres for Admin 1 Association
+        for ($i = 1; $i <= 3; $i++) {
+            $member = User::create([
+                'name' => "Membre Admin1 $i",
+                'email' => "membre_admin1_$i@asso.com",
+                'phone' => "06000001" . $i,
+                'password' => Hash::make('password'),
+                'association_id' => $assoAdmin1->id,
+            ]);
+            $member->assignRole('membre');
+        }
+
+        // Create Membres for Admin 2 Association
+        for ($i = 1; $i <= 3; $i++) {
+            $member = User::create([
+                'name' => "Membre Admin2 $i",
+                'email' => "membre_admin2_$i@asso.com",
+                'phone' => "06000002" . $i,
+                'password' => Hash::make('password'),
+                'association_id' => $assoAdmin2->id,
+            ]);
+            $member->assignRole('membre');
+        }
+
+        // Create Membres for Super Admin Association
+        for ($i = 1; $i <= 2; $i++) {
+            $member = User::create([
+                'name' => "Membre Super $i",
+                'email' => "membre_super$i@asso.com",
+                'phone' => "06000003" . $i,
+                'password' => Hash::make('password'),
+                'association_id' => $assoSuper->id,
+            ]);
+            $member->assignRole('membre');
+        }
     }
 }
